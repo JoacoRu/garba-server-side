5682ee60eaebaeafbeaa46934146dbd8d5d6e069
Joaquin Rubek <joacoru@MBP-de-Joaquin.fibertel.com.ar>
2019-10-31 00:30:30 -0300

    final stage

--- services/products.service.js
+++ services/products.service.js
@@ -7,12 +7,14 @@ class ProductsService {
         try {
             response =  await axios.get(`http://garbarino-mock-api.s3-website-us-east-1.amazonaws.com/products/${productId}`);
             if(!response) return response.data;
-            const enabled =  await EnabledService.get(productId);
-            const responseEnabled = response.data['enabled_for_sale'];
+            var enabled =  await EnabledService.get(productId);
+            var responseEnabled = response.data['enabled_for_sale'];
             if (!enabled) {
                 await EnabledService.create({"productId": productId, enabled: responseEnabled});
             }
+            response.data.enabled_for_sale = enabled.enabled;
         } catch (err) {
+            console.error(err);
             return err;   
         }
         return response.data;
